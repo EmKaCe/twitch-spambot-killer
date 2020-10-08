946629bcf3dc429a0fcd574ed13f59e2db6f28e6
Emre Kaan Çetin <20030146+EmKaCe@users.noreply.github.com>
2020-10-08 16:38:02 +0200

    bug fix, updated ping command!

--- index.js
+++ index.js
@@ -18,6 +18,69 @@ const app = express();
 
 let server;
 
+const runBot = () => {
+    if (server) {
+        server.close();
+    }
+
+    let accessToken = config.accessToken;
+    const refreshToken = config.refreshToken;
+    const channels = config.channels;
+
+    const authProvider = new RefreshableAuthProvider(
+        new StaticAuthProvider(clientId, accessToken),
+        {
+            clientSecret,
+            refreshToken,
+            onRefresh: (token) => {
+                config.accessToken = token;
+                accessToken = token;
+                fs.writeFileSync("./config.json", JSON.stringify(config, null, 4));
+            }
+        }
+    );
+
+    const chatClient = new ChatClient(authProvider, { channels: config.channels });
+
+    let preventing = false;
+
+    const preventSpam = (channel) => {
+        if (!preventing) {
+            chatClient.enableFollowersOnly(channel, 10);
+            preventing = true;
+            setTimeout((channel) => {
+                if (preventing) {
+                    chatClient.disableFollowersOnly(channel);
+                    preventing = false;
+                }
+            }, 600000);
+        }
+    };
+
+    chatClient.onRegister(async () => {
+        console.log("Registered!");
+        await chatClient.join(channels[0]);
+    });
+
+    chatClient.onMessage(async (channel, user, message, msg) => {
+        if (message.search("follow") !== -1) {
+            if (message.search(/https?:\/\//) !== -1 || message.search(/\..?com/) !== -1) {
+                await chatClient.ban(channel, user, "AUTOBAN: Spambot detected!");
+                if (followerOnly) {
+                    preventSpam(channel);
+                }
+            }
+        } else {
+            if (message === "!ping") {
+                chatClient.say(channel, "Anti-Spam: I'll be watching over you!");
+            }
+        }
+    });
+
+    chatClient.connect();
+
+};
+
 if (!config.refreshToken) {
     app.get("/" + pathname + "/add", (req, res) => {
         res.redirect("https://id.twitch.tv/oauth2/authorize?" +
@@ -70,61 +133,3 @@ if (!config.refreshToken) {
 } else {
     runBot();
 }
-
-const runBot = () => {
-    if (server) {
-        server.close();
-    }
-
-    let accessToken = config.accessToken;
-    const refreshToken = config.refreshToken;
-    const channels = config.channels;
-
-    const authProvider = new RefreshableAuthProvider(
-        new StaticAuthProvider(clientId, accessToken),
-        {
-            clientSecret,
-            refreshToken,
-            onRefresh: (token) => {
-                config.accessToken = token;
-                accessToken = token;
-                fs.writeFileSync("./config.json", JSON.stringify(config, null, 4));
-            }
-        }
-    );
-
-    const chatClient = new ChatClient(authProvider, { channels: config.channels });
-
-    let preventing = false;
-
-    const preventSpam = (channel) => {
-        if (!preventing) {
-            chatClient.enableFollowersOnly(channel, 10);
-            preventing = true;
-            setTimeout((channel) => {
-                if (preventing) {
-                    chatClient.disableFollowersOnly(channel);
-                    preventing = false;
-                }
-            }, 600000);
-        }
-    };
-
-    chatClient.onMessage(async (channel, user, message, msg) => {
-        if (message.search("follow") !== -1) {
-            if (message.search(/https?:\/\//) !== -1 || message.search(/\..?com/) !== -1) {
-                await chatClient.ban(channel, user, "AUTOBAN: Spambot detected!");
-                if (followerOnly) {
-                    preventSpam(channel);
-                }
-            }
-        } else {
-            if (message === "?ping") {
-                chatClient.say(channel, "Pong!");
-            }
-        }
-    });
-
-    chatClient.connect();
-
-};
